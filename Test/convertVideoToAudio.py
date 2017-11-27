1c4ffc000e245f3ef516d2b60b0dcf219235991e
Josh Phillips <joshua.phillips@stelligent.com>
2017-11-27 12:09:59 -0800

    converting audio to Video - cleaning up repo

--- Test/convertVideoToAudio.py
+++ Test/convertVideoToAudio.py
@@ -1,11 +1,17 @@
-import ffmpeg
 import argparse
+import ffmpeg
 
 PARSER = argparse.ArgumentParser(description='Convert Video to Audio using ffmpeg API')
 PARSER.add_argument('--i', metavar='i', type=str, nargs=1)
 ARGS = PARSER.parse_args()
 VIDEO = ffmpeg.input(ARGS.i[0])
 
-def convertVideoToAudio(VIDEO):
-    VIDEO.output(ARGS.i[0] + '.mp3').run()
+def convert_video_to_audio(video_file):
+    '''convert Video File to Audio'''
+    video_file.output(ARGS.i[0] + '.mp3').run()
     return
+
+def main():
+    convert_video_to_audio(VIDEO)
+
+if __name__ == "__main__": main()
