5c78a5e4364a47a328f763447d16f59531f7c38a
Josh Phillips <joshua.phillips@stelligent.com>
2017-11-27 12:06:53 -0800

    push convert Video to Audio

--- Test/convertVideoToAudio.py
+++ Test/convertVideoToAudio.py
@@ -4,5 +4,8 @@ import argparse
 PARSER = argparse.ArgumentParser(description='Convert Video to Audio using ffmpeg API')
 PARSER.add_argument('--i', metavar='i', type=str, nargs=1)
 ARGS = PARSER.parse_args()
-print ARGS.i[0]
 VIDEO = ffmpeg.input(ARGS.i[0])
+
+def convertVideoToAudio(VIDEO):
+    VIDEO.output(ARGS.i[0] + '.mp3').run()
+    return
