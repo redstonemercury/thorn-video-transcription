08602e6c4aabadb75d65aaa1ee7507364b0617f8
Chris Jacobs <cjacobs@cylance.com>
2017-11-27 16:55:04 -0800

    gettin classy, yo

--- Test/convertVideoToAudio.py
+++ Test/convertVideoToAudio.py
@@ -1,32 +1,72 @@
 #!/usr/bin/env python3
+
 import sys
 import ffmpeg
-
+from io import BytesIO
+import boto3
 from pydub import AudioSegment
 
 
-CHUNK_SIZE = 15
-DEFAULT_VIDEO = ffmpeg.input(sys.argv[1])
+class Converter(object):
+    CHUNK_SIZE = 15000
+    DEFAULT_VIDEO = ffmpeg.input(sys.argv[1])
+    REGION = 'us-east-1'
+    PROFILE = 'hackathon'
+    VIDEO_BUCKET_NAME = 'sonicthorn-source'
+    AUDIO_BUCKET_NAME = 'sonicthorn-audio'
+    TEXT_BUCKET_NAME = 'sonicthorn-text'
 
+    def __init__(self, region=REGION, profile=PROFILE):
+        self.region = region
+        self.profile = profile
+        self._session = None
+        self._s3_connection = None
+        self._audio_bucket = None
+        self._text_bucket = None
+        self._source_bucket = None
 
-def get_audio(video_file=DEFAULT_VIDEO):
-    '''convert Video File to Audio'''
-    mp3_file = '.'.join(sys.argv[1].split('.')[:-1] + ['mp3'])
-    video_file.output().run()
-    return mp3_file
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = boto3.Session(region_name=self.region, profile_name=self.profile)
+        return self._session
 
+    @property
+    def s3_connection(self):
+        if self._s3_connection is None:
+            self._s3_connection = self.session.client('s3')
+        return self._s3_connection
 
-def chunk_audio(file_name):
-    '''chunk audio file into 15 second segments'''
-    sound = AudioSegment.from_mp3(file_name)
-    for i in range((sound.duration_seconds+1)//CHUNK_SIZE):
-        with open('sound-{}.mp3'.format(i), 'wb') as f:
-            sound.export(f, format='mp3')
+    def upload_to_s3(self, bucket, name, content):
+        self.s3_connection.put_object(Bucket=bucket, Key=name, Body=content)
+
+    def get_audio(self, video_file=DEFAULT_VIDEO):
+        '''convert Video File to Audio'''
+        mp3_file = '.'.join(sys.argv[1].split('.')[:-1] + ['mp3'])
+        video_file.output(mp3_file).run()
+        return mp3_file
+
+    @staticmethod
+    def file_chunk_name(orig, i):
+        parts = orig.split('.')
+        parts.insert(-1, str(i))
+        print('.'.join(parts))
+        return '.'.join(parts)
+
+    def chunk_audio(self, file_name):
+        '''chunk audio file into 15 second segments'''
+        sound = AudioSegment.from_mp3(file_name)
+        for i in range(int(sound.duration_seconds+1)//int(self.CHUNK_SIZE/1000)):
+            chunk_stream = BytesIO()
+            chunk = sound[i*self.CHUNK_SIZE:(i+1)*self.CHUNK_SIZE].export(chunk_stream, format='mp3')
+            chunk_name = self.file_chunk_name(file_name, i)
+            self.upload_to_s3(self.AUDIO_BUCKET_NAME, chunk_name, chunk)
 
 
 def main():
-    mp3_file = get_audio()
-    chunk_audio(mp3_file)
+    c = Converter()
+    mp3_file = c.get_audio()
+    c.chunk_audio(mp3_file)
 
 
 def handler(event, context):
