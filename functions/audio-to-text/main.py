c1b285b6345de58e650bb31a496a270deb1450db
Jessica Major <petralfire@deviantart.com>
2017-11-27 16:23:32 -0800

    Audio to text lambda function

--- functions/audio-to-text/main.py
+++ functions/audio-to-text/main.py
@@ -1,2 +1,17 @@
+import boto3
+
+client = boto3.client('lex-runtime')
+s3 = boto3.resource('s3')
+
+def convert_audio_to_text(input_file):
+    s3.Bucket('sonicthorn-audio').download_file(input_file, '/tmp/audio_file')
+
+    with open('/tmp/audio_file', 'rb') as f:
+        output = client.post_content(botName='sonicthorn', botAlias='processaudio', userId='UserOne', contentType='audio/l16; rate=16000; channels=1', accept='audio/pcm', inputStream=f)
+        return output
+
+
 def handle(event, context):
-    print('Converting audio file to text.')
+    output = convert_audio_to_text((event['audio_file']))
+    print(output)
+
